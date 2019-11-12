4f341fc1a5e69d95b7cb4454840c8a0d1ffff9c5
Bvekv1 <bvekp1@gmail.com>
2019-11-12 21:52:33 +0545

    Er

--- app/src/main/java/com/work/view/fragment/LoginFragment.java
+++ app/src/main/java/com/work/view/fragment/LoginFragment.java
@@ -1,20 +1,30 @@
 package com.work.view.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+
+import com.work.view.DashboardActivity;
 import com.work.view.R;
+import com.work.view.logic.CheckLogin;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
+    private EditText etLoginUsername, etLoginPassword;
+    private Button btnLogin;
+    private String loginUsername, loginPassword;
+    private boolean result;
 
 
     public LoginFragment() {
@@ -23,10 +33,48 @@ public class LoginFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        etLoginUsername = view.findViewById(R.id.etLoginUsername);
+        etLoginPassword = view.findViewById(R.id.etLoginPassword);
+        btnLogin = view.findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUsername = etLoginUsername.getText().toString();
+                loginPassword = etLoginPassword.getText().toString();
+
+                if (!TextUtils.isEmpty(loginUsername) && !TextUtils.isEmpty(loginPassword)) {
+                    CheckLogin CheckLogin = new CheckLogin();
+                    CheckLogin.setCheckLoginname(loginUsername);
+                    CheckLogin.setCheckPassword(loginPassword);
+                    result = CheckLogin.check();
+
+                    if (result) {
+                        etLoginUsername.getText().clear();
+                        etLoginPassword.getText().clear();
+
+                        Intent intent = new Intent(getActivity(), DashboardActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getActivity(), "Username or password is invalid", Toast.LENGTH_LONG).show();
+                        etLoginUsername.getText().clear();
+                        etLoginPassword.getText().clear();
+                    }
+                } else {
+                    if (TextUtils.isEmpty(loginUsername)) {
+                        etLoginUsername.setError("Please enter  user name");
+                    }
+                    if (TextUtils.isEmpty(loginPassword)) {
+                        etLoginPassword.setError("Please enter  password");
+                    }
+                }
+            }
+        });
+        return view;
     }
 
 }
--- app/src/main/java/com/work/view/fragment/RegisterFragment.java
+++ app/src/main/java/com/work/view/fragment/RegisterFragment.java
@@ -2,20 +2,31 @@ package com.work.view.fragment;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.work.view.R;
+import com.work.view.logic.CheckLogin;
+import com.work.view.model.Users;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
-
+    private EditText etRegisterUsername, etRegisterPassword, etConfirmPassword;
+    private Button btnRegister;
+    private String username, password, cPassword;
+    private List<Users> usersList = new ArrayList<>();
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -25,8 +36,50 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
-    }
+        EditText text;
 
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        etRegisterUsername = view.findViewById(R.id.etRegisterUsername);
+        etRegisterPassword = view.findViewById(R.id.etRegisterPassword);
+        etConfirmPassword = view.findViewById(R.id.etConfirmPassword);
+        btnRegister = view.findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = etRegisterUsername.getText().toString();
+                password = etRegisterPassword.getText().toString();
+                cPassword = etConfirmPassword.getText().toString();
+
+                if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(cPassword)){
+                    if (!password.equals(cPassword)){
+                        etConfirmPassword.setError("Password does not match");
+                    } else {
+
+                        Users users = new Users();
+                        users.setUser(username);
+                        users.setUserPassword(password);
+                        usersList.add(users);
+
+                        CheckLogin checkUser = new CheckLogin();
+                        checkUser.setUserListLogin(usersList);
+
+                        Toast.makeText(getActivity(),"Registration Successful", Toast.LENGTH_LONG).show();
+                        etRegisterUsername.getText().clear();
+                        etRegisterPassword.getText().clear();
+                        etConfirmPassword.getText().clear();
+                    }
+                } else {
+                    if (TextUtils.isEmpty(username)){
+                        etRegisterUsername.setError("Please enter User Name");
+                    } if (TextUtils.isEmpty(password)){
+                        etRegisterPassword.setError("Please enter  Password");
+                    } if (TextUtils.isEmpty(cPassword)){
+                        etConfirmPassword.setError("Confirm password");
+                    }
+                }
+            }
+        });
+        return view;
+    }
 }
